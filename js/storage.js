647242aea7b8e913016f5dd9ed0796e7622b864e
Jee <182109647+Aicyraa@users.noreply.github.com>
2025-11-23 19:23:02 +0800

    storage obj

---
+++ js/storage.js
@@ -0,0 +1,26 @@
+const storage = {
+   key: "habit__storage__engine",
+   load() {
+      try {
+         const rawdata = localStorage.getItem(this.key);
+         return rawdata ? JSON.parse(rawdata) : [];
+      } catch (e) {
+         console.log("Error while loading data! " + e);
+         return [];
+      }
+   },
+
+   save(list) {
+      try {
+         localStorage.setItem(this.key, JSON.stringify(list));
+      } catch (e) {
+         console.log("Error while loading saving data! " + e);
+      }
+   },
+
+   clear() {
+      localStorage.removeItem(this.key);
+   },
+};
+
+export default storage;
